3c46cd6f1d41b92cd8a364fd8a86239766115796
Adiouf <Soxnaalimadiouf@gmail.com>
2025-02-21 12:52:47 +0100

    blog_project

--- blog/urls.py
+++ blog/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('blog/', blog, name='about'),
 ]
 
+
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Post
 from django import forms
+from django.urls import reverse
 
 
-def home(request):
-    return render(request, 'blog/index.html')
 
 def blog(request):
-    return render(request, 'blog/blog.html')
-    
-def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.all().order_by('-created_at')  
+    return render(request, 'blog/blog.html', {'posts': posts})
 
 def details_posts(request, id):
     post = get_object_or_404(Post, id=id)  
     return render(request, 'blog/posts_detail.html', {'post': post})
+
+def home(request):
+    return render(request, 'blog/index.html')
 
 class PostForm(forms.Form):
     title = forms.CharField(max_length=200)
@@ -25,13 +25,19 @@ def form_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()  
+            # Créer un nouveau post
             Post.objects.create(
                 title=form.cleaned_data['title'],
                 content=form.cleaned_data['content']
             )
-            return redirect('post_list') 
+            
+            return HttpResponseRedirect(reverse('post_list')) 
     else:
         form = PostForm()
     
     return render(request, 'blog/formulaire_post.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
